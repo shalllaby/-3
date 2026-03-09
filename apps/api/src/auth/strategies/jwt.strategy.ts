d3e4ab37e60f67de28eea5f6861901d5cd59b045
Mohamed Shalaby <shalllaby1@gmail.com>
2026-03-09 04:26:22 +0200

    fix(api): fallback for JWT_SECRET in case it's missing from env variables

--- apps/api/src/auth/strategies/jwt.strategy.ts
+++ apps/api/src/auth/strategies/jwt.strategy.ts
@@ -16,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: config.get<string>('JWT_SECRET'),
+            secretOrKey: config.get<string>('JWT_SECRET') || 'aldeera-pack-super-secret-key-2026',
         });
     }
 
